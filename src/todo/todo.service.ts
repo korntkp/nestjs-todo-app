f2fff3c5e14309ac1736ffcafb1360573d48bb50
Tanakorn Paorat <tanakorn.paorat@gmail.com>
2022-11-25 22:57:54 +0700

    feat: Implement todo CRUD, Add check not found in service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
+  todoList: Todo[] = [];
+
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    const entity: Todo = {
+      id: this.todoList.length,
+      name: createTodoDto.name,
+      description: createTodoDto.description,
+    };
+    this.todoList.push(entity);
+    return entity;
   }
 
   findAll() {
-    return `This action returns all todo`;
+    return this.todoList;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} todo`;
+    const todo = this.todoList.find((todo) => todo.id === id);
+
+    if (!todo) {
+      throw new NotFoundException(`Todo ID: ${id} not found`);
+    }
+
+    return todo;
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+    const todoEntityIndex = this.todoList.findIndex((todo) => todo.id === id);
+
+    if (todoEntityIndex < 0) {
+      throw new NotFoundException(`Todo ID: ${id} not found`);
+    }
+
+    const entity: Todo = {
+      id: this.todoList[todoEntityIndex].id,
+      name: updateTodoDto.name || this.todoList[todoEntityIndex].name,
+      description:
+        updateTodoDto.description || this.todoList[todoEntityIndex].description,
+    };
+
+    this.todoList[todoEntityIndex] = entity;
+    return entity;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} todo`;
+    const todoEntityIndex = this.todoList.findIndex((todo) => todo.id === id);
+
+    if (todoEntityIndex < 0) {
+      throw new NotFoundException(`Todo ID: ${id} not found`);
+    }
+
+    this.todoList.splice(todoEntityIndex, 1);
+    return `Deleted Todo ID: ${id} success`;
   }
 }
